2a9026c3c78d54a72ea75ead92b20e9740fa6d8c
Raghav Sharma <raghavsharmaa1304@gmail.com>
2024-09-18 18:58:46 +0530

    add 01-Largest Number

---
+++ 2k24/01 September/01-Largest Number/code.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    static bool cmp(int & a, int & b){
+        string sa=to_string(a);
+        string sb=to_string(b);
+        return sa+sb>sb+sa;
+    }
+    string largestNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end(), cmp);
+        string res="";
+        if(nums[0]==0){return "0";}
+        for(auto it: nums){
+            res+=to_string(it);
+        }
+        return res;
+
+    }
+};
+int main(){
+
+}
